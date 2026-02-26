008065480b5aa487c8f71975eeb66a62b3c22d0b
OllieHopkinson <8026@balcarras.gloucs.sch.uk>
2026-02-26 22:20:47 +0000

    save2

---
+++ database.py
@@ -0,0 +1,37 @@
+import sqlite3 as sql
+
+class DatabaseHandler:
+    def __init__(self, dbName = 'app.db'):
+        self.dbName = dbName
+
+    def connect(self):
+        return sql.connect(self.dbName)
+        
+
+    def createTables(self):
+        with self.connect() as con:
+            #This is where we create the tables for the student and instructors
+            con.execute('''CREATE TABLE IF NOT EXISTS students(
+                        studentId INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT NOT NULL UNIQUE,
+                        password TEXT NOT NULL,
+                        email TEXT NOT NULL UNIQUE);''')
+            
+            con.execute('''CREATE TABLE IF NOT EXISTS instructors(
+                        instructorId INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT NOT NULL UNIQUE,
+                        password TEXT NOT NULL,
+                        email TEXT NOT NULL UNIQUE);''')
+            
+    #Here is where the functions are to create a student or instructor they take in the username password and email and add them to the database
+    def createStudent(self, username, password, email):
+        with self.connect() as con:
+            con.execute('''INSERT INTO students(username, password, email) VALUES (?, ?, ?)''', (username, password, email))
+            
+            
+    def createInstructor(self, username, password, email):
+        with self.connect() as con:
+            con.execute('''INSERT INTO instructors(username, password, email) VALUES (?, ?, ?)''', (username, password, email))
+                        
+
+    
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+#These are imports from the flask plugin which allow us to create a web server and render html templates and get data from forms
 from flask import Flask, render_template, request
+#These are imports from the database handler file which allows us to interact with the database and create tables and add users to the database
+from database import DatabaseHandler
 
 app = Flask(__name__)
 
+#These lists are used to see if the user name or passwords have any special characters or numbers in
 special_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+',]
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 # Route for the signin page. Displays form for existing users to log in.
@@ -21,21 +25,34 @@ def dashboard():
 
 @app.route('/auth/create_user', methods=['POST'])
 def create_user():
+    #This function gets all the data from the sing in/up form and stores it to be checked against the requirements
     formDetails = request.form
     username = formDetails.get('username')
     password = formDetails.get('password')
     confirm_password = formDetails.get('confirm_password')
     email = formDetails.get('email')
+    user_type = formDetails.get('user_type')
 
+    # This does server side checks and validates that anything inputed is in the right format and meets the requirements
     if (len(username) >= 3 and len(username) <= 10 
         and len(password) >= 4 
-        and len(confirm_password) >= 4 
+        and len(confirm_password) >= 4
         and password == confirm_password
+        and user_type in ['student', 'instructor']
         and not any(char in special_characters for char in username)
         and any(char in numbers for char in password)
         and any(char in special_characters for char in password)):
-        return render_template('dashboard.html')
+            #This add the users data to the database if all checks are passed
+            db = DatabaseHandler()
+            if user_type == 'student':
+                db.createStudent(username, password, email)
+            else:
+                db.createInstructor(username, password, email)
+            return render_template('dashboard.html')
         
     return 'failed to create user'
+
+db = DatabaseHandler()
+db.createTables()
 
 app.run(debug=True)
